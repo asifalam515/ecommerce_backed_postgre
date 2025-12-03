193928eca0a8ed4d02f674c31098d74f9629e44f
asifalam515 <asibulalam515@gmail.com>
2025-12-03 10:41:49 +0600

    put request updated

--- src/modules/seller/seller.route.ts
+++ src/modules/seller/seller.route.ts
@@ -20,6 +20,7 @@ sellerRoute.post("/", async (req: Request, res: Response) => {
     });
   }
 });
+// get all sellers data
 sellerRoute.get("/", async (req: Request, res: Response) => {
   try {
     const allSellers = await pool.query(`SELECT * FROM sellers`);
@@ -35,5 +36,50 @@ sellerRoute.get("/", async (req: Request, res: Response) => {
     });
   }
 });
-// get all sellers data
+// get single seller data by using id
+
+sellerRoute.get("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const seller = await pool.query(`SELECT * FROM sellers WHERE id=$1 `, [id]);
+    if (seller.rows.length === 0) {
+      res.send("no seller found");
+    }
+    res.status(200).json({
+      success: true,
+      data: seller.rows[0],
+      message: "all sellers retrieved",
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// update seller data
+sellerRoute.put("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { name, email, address } = req.body;
+  try {
+    const seller = await pool.query(
+      `UPDATE sellers SET name=$1,email=$2,address=$3 WHERE id=$4 RETURNING *`,
+      [name, email, address, id]
+    );
+    if (seller.rows.length === 0) {
+      res.send("update wasn't successful");
+    }
+    res.status(200).json({
+      success: true,
+      data: seller.rows[0],
+      message: "Updated seller Data",
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 // sellerRoute.get("");
